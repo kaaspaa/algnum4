deb72c4e07a2e413a124beb8b4186747e5c506ef
kaaspaa <kasper12.01@wp.pl>
2019-01-11 22:17:36 +0100

    added Test class with counting/writing time of methods

--- Main.java
+++ Main.java
@@ -1,7 +1,14 @@
 import classes.*;
+import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) {
+    	Test t1 = new Test();
+    	try {
+    		t1.countAndWriteTimeOfExecition("Wyniki czasow");
+		} catch (Exception e) {
+    		System.out.println("błąd przy zapisie pliku");
+		}
     }
 }
---
+++ classes/Test.java
@@ -0,0 +1,56 @@
+package classes;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Test {
+
+    public MyMatrix<Double> countResultsGauss(int numberOfAgents) {
+                int iloscAgentow = numberOfAgents;
+
+                AgentMatrix am = new AgentMatrix(iloscAgentow);
+                am.fulfillMatrix();
+
+                MyMatrix<Double> results = new MyMatrix<Double>(Double.class,(numberOfAgents+1)*(numberOfAgents+2)/2,1);
+                am.countResultVector();
+                results = am.countSecondBGauss();
+                return results;
+        }
+
+        public MyMatrix<Double> countResultsGaussSeidel(int numberOfAgents){
+                int iloscAgentow = numberOfAgents;
+                int iloscProb = 200;
+
+                GaussSeidel gs = new GaussSeidel(iloscAgentow,iloscProb);
+                MyMatrix<Double> results = new MyMatrix<Double>(Double.class,(numberOfAgents+1)*(numberOfAgents+2)/2,1);
+                gs.countGaussSeidelVector();
+                results = gs.countSecondB();
+                return results;
+        }
+
+	public void countAndWriteTimeOfExecition(String fileName) throws IOException {
+        int n=25;
+	    double startTime1, endTime1, startTime2, endTime2, startTime3, endTime3, startTime4, endTime4;
+	    FileWriter fileWriter = new FileWriter(fileName);
+	    PrintWriter printWriter = new PrintWriter(fileWriter);
+	    printWriter.println("Gauss;GaussSeidel;n = " + n + ";");
+    //for (int n=3;n<=30;n++) {
+        //printWriter.println("Gauss;GaussSeidel;n = " + n + ";");
+
+        //for (int q = 1; q <= 10; q++) {
+            //Gauss
+            startTime1 = System.nanoTime();
+            countResultsGauss(n);
+            endTime1 = System.nanoTime();
+            //GaussSeidel
+            startTime2 = System.nanoTime();
+            countResultsGaussSeidel(n);
+            endTime2 = System.nanoTime();
+            printWriter.println((endTime1 - startTime1) + ";" + (endTime2 - startTime2) + ";");
+            //}
+    //}
+                printWriter.close();
+        }
+
+}
